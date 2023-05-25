43dbef2f010e8fa3d843568c5effb86ceb4bd283
Eric Alekseyev <ericalex0207@gmail.com>
2023-05-26 00:23:56 +0300

    ready lesson 24

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - private - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -36,9 +13,29 @@ const personalMovieDB = {
 const countQuestions = 2;
 
 for (let i = 0; i < countQuestions; i++) {
-	const lastWatchFilm = prompt('Один из последних просмотренных фильмов?', '');
-	const ratingFilms = +prompt('На сколько оцените его?', '');
-	personalMovieDB.movies[lastWatchFilm] = ratingFilms;
+	const lastWatchFilm = prompt('Один из последних просмотренных фильмов?', ''),
+		ratingFilms = prompt('На сколько оцените его?', '');
+
+	if (lastWatchFilm !== '' && lastWatchFilm !== null && lastWatchFilm.length <= 50 && ratingFilms !== null &&
+		!isNaN(ratingFilms) && ratingFilms !== '') {
+		personalMovieDB.movies[lastWatchFilm] = ratingFilms;
+		console.log('correct');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+
+const watchedMoverCount = personalMovieDB.count;
+
+if (watchedMoverCount < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (watchedMoverCount >= 10 && watchedMoverCount <= 30) {
+	console.log('Вы классический зритель');
+} else if (watchedMoverCount > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
